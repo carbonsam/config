0a77b23b9eda83c9ddb88db06474d501979a5f4a
Samuel Schumacher <samuelschu@protonmail.com>
2020-01-08 20:08:26 -0600

    Add basic Git config

---
+++ git-config.sh
@@ -0,0 +1,9 @@
+# Set these before running!
+git config --global user.name ""
+git config --global user.email ""
+
+# Match local branches with remotes by default
+git config --global push.default current
+
+# Disable paging for branch listing
+git config --global pager.branch false
